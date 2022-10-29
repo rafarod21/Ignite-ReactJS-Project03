f43c28b2ba5fbe3e9c5ed4e183abb31a2a887186
Rafael Rodrigues <rafael-rafa2.1@hotmail.com>
2022-10-29 13:44:30 -0300

    Create Visual Structure - Transactions Page

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { HeaderContainer, HeaderContent, NewTransitionButton } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt='DT Money' />
+        <NewTransitionButton>Nova transação</NewTransitionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  );
+}
