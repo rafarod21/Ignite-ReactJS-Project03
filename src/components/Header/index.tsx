ef7c8bdf54fdafa27e6a0a20dbb0bc967f060038
Rafael Rodrigues <rafael-rafa2.1@hotmail.com>
2022-11-27 20:02:11 -0300

    Create Dialog

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,24 @@
-import { HeaderContainer, HeaderContent, NewTransitionButton } from './styles';
+import * as Dialog from '@radix-ui/react-dialog';
+
+import { NewTransactionModal } from '../NewTransactionModal';
 
 import logoImg from '../../assets/logo.svg';
+
+import { HeaderContainer, HeaderContent, NewTransitionButton } from './styles';
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt='DT Money' />
-        <NewTransitionButton>Nova transação</NewTransitionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransitionButton>Nova transação</NewTransitionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
